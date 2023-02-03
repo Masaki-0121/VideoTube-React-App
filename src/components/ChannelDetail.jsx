801a48924b03410bece0cf468861aa867fb8c508
masaki <j661masaki@gmail.com>
2023-02-03 20:34:52 +0900

    channel detail and card size arranged

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/system";
 
-import { Videos, channelCard } from "./";
+import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
@@ -22,7 +22,24 @@ const ChannelDetail = () => {
       (data) => setVideos(data?.items)
     );
   }, [id]);
-  return <div>{id}</div>;
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 };
-
 export default ChannelDetail;
